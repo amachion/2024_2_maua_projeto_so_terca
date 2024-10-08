b5f03f8f301f30c8827bc7cadb139c5990c2550d
Andreia <andreia.machion@saojudas.br>
2024-10-08 12:44:51 -0300

    finalizada seção cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //console.log('hello, NodeJs!!!')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 let filmes = [
     {
